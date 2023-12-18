b7741375472a6c1eedf79fb3306e93365862b884
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-12-18 18:42:52 +0500

    add requirements

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c##a5!p$&@w8835l6pe2#%!s86gj#kp!ig9n#gx=@kbi0^$v=k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
