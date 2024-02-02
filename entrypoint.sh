3ec6ed13b1ab83db08e3604782a0cb9f593ea8ea
UmirovJobir <jobirumirovmoliya@gmail.com>
2024-02-02 18:07:02 +0500

    Docker

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$DATABASE" = "Imodels" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+sleep 2
+
+echo "Apply database migrations"
+python3 manage.py migrate
+gunicorn mysite.wsgi:application --bind 0.0.0.0:8000 --timeout 300
+
+exec "$@"
