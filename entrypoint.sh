5f6d7732a0cd6683309c008d3be24034f20a0658
UmirovJobir <jobirumirovmoliya@gmail.com>
2024-09-18 15:13:08 +0500

    deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,6 @@ sleep 2
 
 echo "Apply database migrations"
 python3 manage.py migrate
-gunicorn mysite.wsgi:application --bind 0.0.0.0:8000 --timeout 300
+gunicorn mysite.wsgi:application --bind 0.0.0.0:8081 --timeout 300
 
 exec "$@"
