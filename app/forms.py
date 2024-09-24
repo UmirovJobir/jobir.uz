d84c097b0bd6f8e5cafea7d25f1e2461a394f35f
UmirovJobir <jobirumirovmoliya@gmail.com>
2024-09-24 18:06:23 +0500

    numeric input

--- app/forms.py
+++ app/forms.py
@@ -8,6 +8,12 @@ class GetInTouchForm(forms.ModelForm):
         fields  = '__all__'
         widgets = {
             'name': forms.TextInput(attrs={'placeholder': 'Name'}),
-            'phone': forms.NumberInput(attrs={'placeholder': 'Phone', 'value': '998'}),
+            # 'phone': forms.TextInput(attrs={'placeholder': 'Phone', 'value': '998'}),
+            'phone': forms.NumberInput(attrs={
+                'placeholder': 'Phone', 
+                'value': '998', 
+                'inputmode': 'numeric', 
+                'pattern': '[0-9]*'
+            }),
             'message': forms.Textarea(attrs={'placeholder': 'Project details'}),
         }
